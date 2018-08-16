f4dfefffac56f6cf478597a501da8a96c7242cee
Kirill Sulim <sulim@skbkontur.ru>
2018-08-16 14:14:51 +0500

    Add mockito annotations

--- src/main/java/io/su0/test/soccer/service/GroupService.java
+++ src/main/java/io/su0/test/soccer/service/GroupService.java
@@ -1,6 +1,10 @@
 package io.su0.test.soccer.service;
 
+import io.su0.test.soccer.domain.Group;
+import io.su0.test.soccer.persistence.GroupRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * GroupService
@@ -9,4 +13,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GroupService {
+
+    private final GroupRepository groupRepository;
+
+    public GroupService(GroupRepository groupRepository) {
+        this.groupRepository = groupRepository;
+    }
+
+    public List<Group> getGroups() {
+        return groupRepository.findAll();
+    }
 }
--- src/test/java/io/su0/test/soccer/service/GroupServiceTest.java
+++ src/test/java/io/su0/test/soccer/service/GroupServiceTest.java
@@ -1,14 +1,34 @@
 package io.su0.test.soccer.service;
 
+import io.su0.test.soccer.domain.Group;
+import io.su0.test.soccer.persistence.GroupRepository;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GroupServiceTest {
 
+    @Mock
+    private GroupRepository groupRepository;
+
+    @InjectMocks
+    private GroupService groupService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
     void name() {
-        Assertions.assertTrue(true);
+        List<Group> groups = groupService.getGroups();
     }
 }
