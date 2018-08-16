619c9ecb1799e3c51a2f48a64601b915406eb6b5
Kirill Sulim <sulim@skbkontur.ru>
2018-08-16 14:32:46 +0500

    Add integration test example

---
+++ src/test/java/io/su0/test/soccer/service/AIT.java
@@ -0,0 +1,17 @@
+package io.su0.test.soccer.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * AIT
+ *
+ * @author Kirill Sulim
+ */
+public class AIT {
+
+    @Test
+    void name() {
+        Assertions.fail("Faill from it");
+    }
+}
