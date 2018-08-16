2e234a809736e15d8a028bacf0281fb4c9fc9eff
Kirill Sulim <sulim@skbkontur.ru>
2018-08-16 18:55:47 +0500

    WIP

--- src/test/java/io/su0/test/soccer/service/GroupServiceTest.java
+++ src/test/java/io/su0/test/soccer/service/GroupServiceTest.java
@@ -1,5 +1,8 @@
 package io.su0.test.soccer.service;
 
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import io.su0.test.soccer.domain.Group;
 import io.su0.test.soccer.persistence.GroupRepository;
 import org.junit.jupiter.api.Assertions;
@@ -9,7 +12,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.testcontainers.containers.DockerComposeContainer;
+import org.testcontainers.containers.GenericContainer;
 
+import java.io.File;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,13 +28,27 @@ class GroupServiceTest {
     @InjectMocks
     private GroupService groupService;
 
+    private DockerComposeContainer soccer;
+
     @BeforeEach
     void setUp() {
+
+        soccer = new DockerComposeContainer<>(new File("src/test/resources/docker-compose-it.yml"))
+                .withExposedService("soccer", 8080);
+
+
+
         MockitoAnnotations.initMocks(this);
     }
 
     @Test
     void name() {
-        List<Group> groups = groupService.getGroups();
+
+        soccer.start();
+
+        HttpResponse<JsonNode> json = Unirest.get("http://localhost:8080/groups").asJson();
+        System.out.println(json);
+
+        soccer.stop();
     }
 }
